77bf2768a8dc32af32dfcbfe87b5d4ffec92aa3b
Sai charan bandi <108537114+Charan2409@users.noreply.github.com>
2023-07-26 00:17:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Spinner from "../src/component/Spinner";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Spinner />
+    </div>
+  );
+}
---
+++ src/component/Spinner.js
@@ -0,0 +1,11 @@
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <>
+      <div className="spinner"></div>
+    </>
+  );
+};
+
+export default Spinner;
